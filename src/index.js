e4bfdca77bcdd9caf2963cbe5263b463ecc3fd39
Lunathii <lunathintonintshi@gmail.com>
2024-10-28 18:56:59 +0200

    adding css and js files
    edit html and insert and add css and js file

---
+++ src/index.js
@@ -0,0 +1,114 @@
+function updateWeatherData(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+  
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `
+      <img
+      src="${response.data.condition.icon_url}"
+      alt=""
+      class="weather-app-emoji"
+      />`;
+  
+    document.body.className = response.data.condition.icon;
+    let body = document.querySelector("body");
+    let icon = response.data.condition.icon;
+    if (icon.includes("night")) {
+      body.classList.add("dark");
+    }
+    getForecast(response.data.city);
+  }
+  
+  function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "tb3fa10906d8aeb018ofce8736864bb2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeatherData);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+  
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+  
+  function getForecast(city) {
+    let apiKey = "tb3fa10906d8aeb018ofce8736864bb2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+  
+  function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let forecastHtml = "";
+  
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml =
+          forecastHtml +
+          `<div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+      </div>
+      <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-max-temperature">
+                <strong>${Math.round(day.temperature.maximum)}°C</strong>
+                </div>
+                <div class="weather-forecast-min-temperature">${Math.round(
+                  day.temperature.minimum
+                )}°C</div>
+                </div>
+                </div>`;
+      }
+    });
+  
+    forecastElement.innerHTML = forecastHtml;
+  }
+  
+  searchCity("Johannesburg");
+  
